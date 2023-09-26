976986dd836afa3c3b635a3c8287dcd503d671d4
wolfiee42 <saifalislam2022@gmail.com>
2023-09-26 10:50:40 +0600

    made the website responsive

--- src/Compoents/NavBar/Navbar.jsx
+++ src/Compoents/NavBar/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="flex justify-between items-center my-5">
+        <div className="flex flex-col gap-5 md:flex-row md:gap-0 justify-between items-center my-5">
             <div>
                 <img src="https://i.ibb.co/6NMHqPt/Logo.png" alt="" />
             </div>
